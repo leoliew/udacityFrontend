0d6261142179909aac099f3aba4746321f1a01b9
Leo <zmliu0077@gmail.com>
2018-11-15 23:58:19 +0800

    finish

--- memory-game_zh/js/app.js
+++ memory-game_zh/js/app.js
@@ -1,33 +1,42 @@
 /*
  * 创建一个包含所有卡片的数组
+ * 设置一张卡片的事件监听器。 如果该卡片被点击：
+ *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
+ *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
+ *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
+ *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
-let card = document.querySelectorAll('.card')
-
+let card = document.getElementsByClassName('card')
 let cards = [...card]
 
-let deck = document.querySelector('.deck')
+let deck = document.getElementById('card-deck')
 
-let matchedCard = document.querySelectorAll('.match')
+let matchedCard = document.getElementsByClassName('match')
 
 let moves = 0
 let counter = document.querySelector('.moves')
 
-// declare variables for star icons
 const stars = document.querySelectorAll('.fa-star')
 
-// close icon in modal
-let closeicon = document.querySelector('.close')
+let closeButton = document.querySelector('.close')
 
-// declare modal
-let modal = document.getElementById('popup1')
+let modal = document.getElementById('finish-popup')
+
+// 设置计时器
+let second = 0
+let minute = 0
+let hour = 0
+let timer = document.querySelector('.timer')
+let interval
 
 // 已经打开的card
 let openedCards = []
 
 function startPlay () {
-    // console.log(deck)
     cards = shuffle(cards)
-    // remove all exisiting classes from each card
     for (let i = 0; i < cards.length; i++) {
         deck.innerHTML = ''
         cards.forEach(item => {
@@ -35,21 +44,20 @@ function startPlay () {
         })
         cards[i].classList.remove('show', 'open', 'match', 'disabled')
     }
-
-    // reset moves
+    // 1.重置步数为0
     moves = 0
     counter.innerHTML = moves
-    // reset rating
+    // 2.重置星星数为三颗
     for (var i = 0; i < stars.length; i++) {
         stars[i].style.color = '#FFD700'
         stars[i].style.visibility = 'visible'
     }
-    // reset timer
+    // 3.重置时间
     second = 0
     minute = 0
     hour = 0
     let timer = document.querySelector('.timer')
-    timer.innerHTML = '0 mins 0 secs'
+    timer.innerHTML = '0 minutes 0 seconds'
     clearInterval(interval)
 }
 
@@ -64,8 +72,7 @@ document.body.onload = startPlay()
 
 // 洗牌函数来自于 http://stackoverflow.com/a/2450976
 function shuffle (array) {
-    var currentIndex = array.length, temporaryValue, randomIndex
-
+    let currentIndex = array.length, temporaryValue, randomIndex
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex)
         currentIndex -= 1
@@ -73,91 +80,62 @@ function shuffle (array) {
         array[currentIndex] = array[randomIndex]
         array[randomIndex] = temporaryValue
     }
-
     return array
 }
 
-/*
- * 设置一张卡片的事件监听器。 如果该卡片被点击：
- *  - 显示卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 将卡片添加到状态为 “open” 的 *数组* 中（将这个功能放在你从这个函数中调用的另一个函数中）
- *  - 如果数组中已有另一张卡，请检查两张卡片是否匹配
- *    + 如果卡片匹配，将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
- *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
- */
-
-// TODO: 开始进行游戏函数
-
-// @description toggles open and show class to display cards
+// 显示卡片
 let displayCard = function () {
     this.classList.toggle('open')
     this.classList.toggle('show')
     this.classList.toggle('disabled')
 }
 
+// 匹配卡片，同时检查是否已经全部匹配
 let isMatch = function () {
-
-    // TODO: isMatch 将卡片锁定为 "open" 状态（将这个功能放在你从这个函数中调用的另一个函数中）
     openedCards[0].classList.add('match', 'disabled')
     openedCards[1].classList.add('match', 'disabled')
     openedCards[0].classList.remove('show', 'open', 'no-event')
     openedCards[1].classList.remove('show', 'open', 'no-event')
-
     openedCards = []
-
-    // TODO:  isMatch 函数顺带检查是否全匹配 , 显示最终分数
-
     if (matchedCard.length === 2) {
         clearInterval(interval)
         let finalTime = timer.innerHTML
-
-        // show congratulations modal
         modal.classList.add('show')
-
-        // declare star rating variable
         let starRating = document.querySelector('.stars').innerHTML
-
-        //showing move, rating, time on modal
         document.getElementById('finalMove').innerHTML = moves
         document.getElementById('starRating').innerHTML = starRating
         document.getElementById('totalTime').innerHTML = finalTime
-
-        //closeicon on modal
         closeModal()
     }
 }
 
-// @description close icon on modal
+// 关闭庆祝框
 function closeModal () {
-    closeicon.addEventListener('click', function (e) {
-        // modal.classList.add("close")
+    closeButton.addEventListener('click', function (e) {
         modal.classList.remove('show')
         startPlay()
     })
 }
 
+// 计算总步数
 let countMoveStep = function () {
     moves++
     counter.innerHTML = moves
-    //start timer on first click
-    if (moves == 1) {
+    if (moves === 1) {
         second = 0
         minute = 0
         hour = 0
         startTimer()
     }
-    // setting rates based on moves
     if (moves > 8 && moves < 12) {
-        for (i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
             if (i > 1) {
                 stars[i].style.visibility = 'collapse'
             }
         }
     }
     else if (moves > 13) {
-        for (i = 0; i < 3; i++) {
+        for (let i = 0; i < 3; i++) {
             if (i > 0) {
                 stars[i].style.visibility = 'collapse'
             }
@@ -165,35 +143,17 @@ let countMoveStep = function () {
     }
 }
 
-// @description game timer
-var second = 0, minute = 0
-hour = 0
-var timer = document.querySelector('.timer')
-var interval
-
+// 开始计时
 function startTimer () {
     interval = setInterval(function () {
-        timer.innerHTML = minute + 'mins ' + second + 'secs'
+        timer.innerHTML = minute + ' minutes ' + second % 60 + ' seconds'
         second++
-        if (second == 60) {
-            minute++
-            second = 0
-        }
-        if (minute == 60) {
-            hour++
-            minute = 0
-        }
+        minute = parseInt(second / 60)
     }, 1000)
 }
 
+// 卡片不匹配执行
 let notMatch = function () {
-
-    console.log(matchedCard.length)
-
-    //TODO : is NotMatch 如果卡片不匹配，请将卡片从数组中移除并隐藏卡片的符号（将这个功能放在你从这个函数中调用的另一个函数中）
-
-    // openedCards[0].classList.remove('open', 'show')
-    // openedCards[1].classList.remove('open', 'show')
     openedCards[0].classList.add('unmatched')
     openedCards[1].classList.add('unmatched')
     disableAllCard()
@@ -206,33 +166,29 @@ let notMatch = function () {
 
 }
 
+// 把所有的卡片设置为不能点击
 let disableAllCard = function () {
     Array.prototype.filter.call(cards, function (card) {
         card.classList.add('disabled')
     })
 }
 
-// @description enable cards and disable matched cards
+// 把剩余未匹配的卡片设置为可点击
 function enable () {
     Array.prototype.filter.call(cards, function (card) {
         card.classList.remove('disabled')
-        for (var i = 0; i < matchedCard.length; i++) {
+        for (let i = 0; i < matchedCard.length; i++) {
             matchedCard[i].classList.add('disabled')
         }
     })
 }
 
+// 打开卡片,在第一次进行匹配的时候开始计数器
 let cardOpen = function () {
     openedCards.push(this)
-    if (openedCards.length === 2) {
-
+    if (openedCards.length === 16) {
         countMoveStep()
-        //TODO： 在打开第一张卡片的时候开始计数器
-        console.log(openedCards[0].title)
-        console.log(openedCards[1].title)
-
         if (openedCards[0].title === openedCards[1].title) {
-            console.log('match')
             isMatch()
         } else {
             console.log('not match')
@@ -242,17 +198,15 @@ let cardOpen = function () {
     }
 }
 
-// TODO: 再次进行游戏 1.把卡片设置为关闭 2.时间重置
+// 再次进行游戏
 let playAgain = function () {
-    // @desciption for user to play Again
-        modal.classList.remove("show");
-        startPlay();
+    modal.classList.remove('show')
+    startPlay()
 }
 
-// loop to add event listeners to each card
+// 绑定事件
 for (let i = 0; i < cards.length; i++) {
     card = cards[i]
     card.addEventListener('click', displayCard)
     card.addEventListener('click', cardOpen)
-    // card.addEventListener("click",congratulations);
 }
